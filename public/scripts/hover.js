7395816eb21884d0b5c44ee29eec092f3b2873a8
devin-web <devin@alumni.sfu.ca>
2016-09-21 02:38:37 +0000

    Day 2 hovering working and reviewed

--- public/scripts/hover.js
+++ public/scripts/hover.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
   $('.committed-tweets article').on('mouseover', function() {
-    console.log("Hover");
     $( this ).find( '.icons' ).css('display', 'initial'); //maybe should be inline?
   });
 
   $('.committed-tweets article').on('mouseout', function() {
     $( this ).find( '.icons' ).css('display', 'none');
-    console.log("no Hover");
   });
 });
